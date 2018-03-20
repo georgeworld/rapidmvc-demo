81a9bd338d48dd3bcfc68b736a07cca10862077a
George <GeorgeWorld@qq.com>
2018-03-20 15:29:59 +0800

    增加更多演示功能

--- src/main/java/com/georgeinfo/rapidmvc/controllers/UserCenterController.java
+++ src/main/java/com/georgeinfo/rapidmvc/controllers/UserCenterController.java
@@ -6,10 +6,12 @@ package com.georgeinfo.rapidmvc.controllers;
 
 import java.io.IOException;
 import java.util.HashMap;
+
 import com.georgeinfo.base.util.encrypt.EncTool;
 import com.georgeinfo.base.util.logger.GeorgeLogger;
 import com.georgeinfo.rapidmvc.BasicController;
 import com.georgeinfo.rapidmvc.annotation.Get;
+import com.georgeinfo.rapidmvc.annotation.Param;
 import com.georgeinfo.rapidmvc.annotation.Path;
 import com.georgeinfo.rapidmvc.annotation.Post;
 import com.georgeinfo.rapidmvc.result.Result;
@@ -17,8 +19,9 @@ import com.georgeinfo.rapidmvc.beans.Message;
 import com.georgeinfo.rapidmvc.beans.MsgHeader;
 import gbt.config.GeorgeLoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
- *
  * @author George <Georgeinfo@163.com>
  */
 @Path("home/user-center")
@@ -92,14 +95,30 @@ public class UserCenterController extends BasicController {
         return rf.view("default-index");
     }
 
-    @Get
-    @Post
-    public Result jsonTest() {
-        HashMap<String, String> m = new HashMap<String, String>();
-        m.put("a", "111");
-        m.put("b", "222");
+    /**
+     * 下面这个控制器方法的例子，同时演示URI参数注入、HttpServletRequest内置对象注入，返回json数据，
+     * 以及使用ac方式接受http参数这几个主要的功能。
+     **/
+    @Get("json-test/{userName}")
+    @Post("json-test/{userName}")
+    public Result jsonTest(@Param("userName") String userName,
+                           @Param("request") HttpServletRequest reqeust) {
+        //使用reqeust接受参数
+        String studio = getRequest().getParameter("studio");
 
-        return rf.json(m);
+        //使用ac方式接受参数
+        String website = ac.getParameter("website");
+
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("用户名", userName);
+        data.put("工作室", studio);
+        data.put("网址", website);
+
+        Message msg = new Message();
+        msg.setHeader(new MsgHeader(200, "处理成功"));
+        msg.setData(data);
+
+        return rf.json(msg);
     }
 
 }
